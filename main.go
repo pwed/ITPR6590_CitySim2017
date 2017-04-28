1ee929351412d05bf9d14a9b35b10ec01fb6ec91
Frederick Stoddart <freddiestoddart000@gmail.com>
2017-04-29 01:13:45 +1200

    Added main.go and .gitignore

---
+++ main.go
@@ -0,0 +1,13 @@
+// A project to simulate traffic navigation in the city of Hastings.
+//
+// Team members: Frederick Stoddart, Levi Fraser-Daley, Nikolas Burke
+package main
+
+import "fmt"
+
+// Main() :p
+func main() {
+
+	fmt.Println("Hello Hastings!")
+
+}
