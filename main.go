d013905e18df1e27460e36b6ce687957b6e505a6
Levi <koenigcharlie@hotmail.com>
2017-04-29 02:38:27 +1200

    Added, FunCity Drivers, array of object

--- main.go
+++ main.go
@@ -5,12 +5,13 @@ package main
 
 import "fmt"
 
-
+// A location representing a suburb in a city
 type funCityLoc struct {
-	name string
+	name        string
 	outsideCity bool
 }
 
+//
 type funStreet struct {
 	name string
 	exit funExit
@@ -20,7 +21,7 @@ type funExit string
 
 type funDriver struct {
 	number int
-	name string
+	name   string
 }
 
 var hastings = []funCityLoc{
@@ -31,9 +32,18 @@ var hastings = []funCityLoc{
 	{"Outside City", true},
 }
 
+var drivers = []funDriver{
+	{1, "Fred"},
+	{2, "Caitlyn"},
+	{3, "Mason"},
+	{4, "Bea"},
+	{5, "Tara"},
+}
+
 // Main() :p
 func main() {
 
 	fmt.Println(hastings)
+	fmt.Println(drivers)
 
 }
