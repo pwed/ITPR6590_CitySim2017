eacf671de0e66825ced6d820f73e5d2eef9ba9b9
Frederick Stoddart <freddiestoddart000@gmail.com>
2017-04-29 01:48:51 +1200

    Added some of the required structs

--- main.go
+++ main.go
@@ -5,9 +5,35 @@ package main
 
 import "fmt"
 
+
+type funCityLoc struct {
+	name string
+	outsideCity bool
+}
+
+type funStreet struct {
+	name string
+	exit funExit
+}
+
+type funExit string
+
+type funDriver struct {
+	number int
+	name string
+}
+
+var hastings = []funCityLoc{
+	{"Mayfair", false},
+	{"Akina", false},
+	{"Stortford Lodge", false},
+	{"Mahora", false},
+	{"Outside City", true},
+}
+
 // Main() :p
 func main() {
 
-	fmt.Println("Hello Hastings!")
+	fmt.Println(hastings)
 
 }
